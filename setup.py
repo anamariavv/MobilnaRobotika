6f7afc25120de0e9a980609ffb586594a4cfedbd
Anamaria <35101063+anamariavv@users.noreply.github.com>
2023-01-20 22:44:43 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -12,18 +12,18 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='anamariav',
-    maintainer_email='anamariav@todo.todo',
+    maintainer='anamaria',
+    maintainer_email='anamaria@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        	"move = project.move:main"
+        	"move = project.move:main",
+        	"image_publisher = project.image_publisher:main"
         ],
     },
 )
